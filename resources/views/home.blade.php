6e701c5528f30c632c2a5d91d830522c4d1b32f7
arsonull <spartancamo117@gmail.com>
2021-04-16 23:17:20 -0700

    home page links work

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -9,35 +9,35 @@
             <h2 class="mb-5">"Apes alone weak. Apes together strong." - Caesar from Rise of the Planet of the Apes</h2>
         </div>
         <div class="row no-gutters">
-            <div class="col-lg-6"><a class="portfolio-item" href="#">
+            <div class="col-lg-6"><a class="portfolio-item" href="/TogetherPeopleStrong/affinities">
                     <div class="caption">
                         <div class="caption-content">
                             <h2>Affinity Groups</h2>
-                            <p class="mb-0">A yellow pencil with envelopes on a clean, blue backdrop!</p>
+                            <p class="mb-0">Join together with other like-minded monkey's in an Affinity Group!</p>
                         </div>
                     </div><img class="img-fluid" src="resources/img/monkeySocial.png">
                 </a></div>
-            <div class="col-lg-6"><a class="portfolio-item" href="#">
+            <div class="col-lg-6"><a class="portfolio-item" href="/TogetherPeopleStrong/job_postings">
                     <div class="caption">
                         <div class="caption-content">
                             <h2>Job Postings</h2>
-                            <p class="mb-0">A dark blue background with a colored pencil, a clip, and a tiny ice cream cone!</p>
+                            <p class="mb-0">Search for work to buy more bananas</p>
                         </div>
                     </div><img class="img-fluid" src="resources/img/monkeySearch.jpg">
                 </a></div>
-            <div class="col-lg-6"><a class="portfolio-item" href="#">
+            <div class="col-lg-6"><a class="portfolio-item" href="/TogetherPeopleStrong/portfolio/viewAll">
                 <div class="caption">
                     <div class="caption-content">
                         <h2>View Portfolios</h2>
-                        <p class="mb-0">Strawberries are such a tasty snack, especially with a little sugar on top!</p>
+                        <p class="mb-0">View what other monkeys are showing off</p>
                     </div>
                 </div><img class="img-fluid" src="resources/img/monkeyLaptop.jpg">
             </a></div>
-            <div class="col-lg-6"><a class="portfolio-item" href="#">
+            <div class="col-lg-6"><a class="portfolio-item" href="/TogetherPeopleStrong/portfolio/create">
                 <div class="caption">
                     <div class="caption-content">
                         <h2>Edit your Portfolio</h2>
-                        <p class="mb-0">A yellow workspace with some scissors, pencils, and other objects.</p>
+                        <p class="mb-0">Manage what YOU are showing off to other monkeys</p>
                     </div>
                 </div><img class="img-fluid" src="resources/img/monkeySuit.jpg">
             </a></div>
--- resources/views/layouts/footer.blade.php
+++ resources/views/layouts/footer.blade.php
@@ -1,5 +1,5 @@
 <footer class="text-center footer">
         <div class="container">
-            <p class="text-muted mb-0 small">Copyright &nbsp;© TogetherPeopleStrong 2021</p>
+            <p class="text-muted mb-0 small">Copyright &nbsp;&#169 TogetherPeopleStrong 2021</p>
         </div><a class="js-scroll-trigger scroll-to-top rounded" href="#page-top"><i class="fa fa-angle-up"></i></a>
 </footer>
